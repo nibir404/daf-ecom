64492614c8136627baa4e03ac3a97c883f933626
Nick404 <nibirimtiaz1@gmail.com>
2026-04-23 15:42:45 +0600

    feat: initialize frontend project with React, Vite, and Tailwind CSS setup

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav>
-      <h1>DAF E-com</h1>
+      <div className='header-main flex justify-between items-center w-full'>
+        <div className='brand-logo flex items-center gap-10'>
+          <img src="./public/Logo Container.png" alt="daf-logo" />
+           <h3 className="nav-item"><a href="#">Start Here</a></h3>
+        </div>
+        <div className='menus '>
+          <ul className='nav-links flex justify-center items-center gap-10'>
+            <li className="nav-item"><a href="#">About Us</a></li>
+            <li className="nav-item"><a href="#">Services</a></li>
+            <li className="nav-item"><a href="#">Pricing</a></li>
+            <li className="nav-item"><a href="#">Resources</a></li>
+          </ul>
+        </div>
+        <div className='header-CTA flex items-center gap-10'>
+         <div className='flex gap-4 '>
+          <h3 className="nav-item"><a href="#">Explore</a></h3>
+          <img src="/more.svg" alt="icon" />
+         </div> 
+          <button className='bg-[#265F58] rounded-[2px] text-white !px-6 !py-2 shadow-[-1px_-1px_0px_0px_rgba(255,255,255,0.25),1px_1px_0px_0px_rgba(255,255,255,0.25)]'>Contact Us</button>
+        </div>
+      </div>
     </nav>
   )
 }
