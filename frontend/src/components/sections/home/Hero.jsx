72e47d3d79814a354c404b3bb7e625b25953ad91
Nick404 <nibirimtiaz1@gmail.com>
2026-04-23 18:23:29 +0600

    feat: add Hero component and associated assets to home page

--- frontend/src/components/sections/home/Hero.jsx
+++ frontend/src/components/sections/home/Hero.jsx
@@ -1,5 +1,18 @@
-import React from 'react';
-
-const Hero = () => { return (<div>Hero</div>); };
+const Hero = () => {
+    return (
+        <section className="bg-center bg-[url('/homehero.svg')] w-[1920px] h-[820px] bg-no-repeat bg-cover relative">
+            <div className="hero-content ">
+                <h1>Hero Section</h1>
+                <p>Hero Section</p>
+            </div>
+           <div>
+             <button className='bg-[#265F58] rounded-[2px] text-white !px-6 !py-2 transition duration-300 ease-in-out hover:bg-[#021B79] flex gap-2 items-center'>
+            Contact Us
+            <img src="/arrow-right.svg" alt="arrow-icon" />
+          </button>
+           </div>
+        </section>
+    );
+};
 
 export default Hero;
