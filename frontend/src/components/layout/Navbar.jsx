078eb76b35c512d56f0b7bf33006af3c86cd199d
Nick404 <nibirimtiaz1@gmail.com>
2026-04-26 18:02:04 +0600

    feat: implement global Tailwind CSS theme and add initial Navbar and Hero components for the homepage

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,17 +1,17 @@
 const Navbar = () => {
   return (
-    <nav>
-      <div className='header-main flex justify-between items-center w-full bg-white/50 backdrop-blur-md !px-50 h-[80px] '>
+    <nav className="sticky top-0 z-50 w-full">
+      <div className='header-main flex justify-between items-center w-full bg-white/50 backdrop-blur-md px-6 md:px-20 lg:px-40 h-[80px]'>
         <div className='brand-logo flex items-center gap-10'>
           <img src="/Logo Container.png" alt="daf-logo" />
            <h3 className="nav-item"><a href="#">Start Here</a></h3>
         </div>
         <div className='menus '>
-          <ul className='nav-links flex justify-center items-center gap-10'>
-            <li className="nav-item"><a href="#">About Us</a></li>
-            <li className="nav-item"><a href="#">Services</a></li>
-            <li className="nav-item"><a href="#">Pricing</a></li>
-            <li className="nav-item"><a href="#">Resources</a></li>
+          <ul className='nav-links flex justify-center items-center'>
+            <li className="nav-item hover:bg-white-50 px-5 py-4 transition duration-300 ease-in-out"><a href="#">About Us</a></li>
+            <li className="nav-item hover:bg-white-50 px-5 py-4 transition duration-300 ease-in-out"><a href="#">Services</a></li>
+            <li className="nav-item hover:bg-white-50 px-5 py-4 transition duration-300 ease-in-out"><a href="#">Pricing</a></li>
+            <li className="nav-item hover:bg-white-50 px-5 py-4 transition duration-300 ease-in-out"><a href="#">Resources</a></li>
           </ul>
         </div>
         <div className='header-CTA flex items-center gap-10'>
--- frontend/src/components/sections/home/Hero.jsx
+++ frontend/src/components/sections/home/Hero.jsx
@@ -1,11 +1,16 @@
 const Hero = () => {
     return (
-        <section className="w-[1920px] h-[850px] bg-center bg-[url('/homehero.svg')] bg-no-repeat flex justify-center items-center">
-            <div className="hero-content flex flex-col items-center justify-center w-[1220px] text-center gap-6 mt-[-300px]">
-                <h1 className="text-[64px] leading-[1.15] tracking-[-0.02em] text-black-900">
-                    Build <p className="inline-block text-black-900 font-semibold">Software,<p className='inline-block text-orange-600 font-bold'> ERP </p> & <p className='inline-block text-black-900 font-bold'>Digital Systems</p>  </p> <br />That Scale Without Rebuilding
+        <section className="w-full h-[850px] bg-center bg-[url('/homepage/home-hero.png')] bg-cover bg-no-repeat flex justify-center items-center overflow-x-hidden">
+            <div className="hero-content flex flex-col items-center justify-center w-[1220px] text-center mt-[-300px]">
+                <div className="flex items-center gap-2.5 bg-gold-50 px-4 py-2.5 rounded-full mb-10">
+                    <img src="/homepage/growth.svg" alt="growth" className="w-4 h-4" />
+                    <span className="text-sm text-gold-800 font-normal leading-none">Optimize your business growth</span>
+                </div>
+
+                <h1 className="text-[64px] leading-[1.15] tracking-[-0.02em] text-black-400 mb-4">
+                    Build <span className="text-black-900 font-bold">Software, <span className='text-orange-600 font-bold'>ERP</span> & <span className='text-black-900 font-bold'>Digital Systems</span></span> <br />That Scale Without Rebuilding
                 </h1>
-                <p className="text-[20px] leading-[1.5] text-black-300">
+                <p className="text-[20px] leading-[1.5] text-black-400 mb-10">
                     From first website to enterprise systems we help you <br />build once and scale without rebuilding.
                 </p>
                 <div className='flex items-center gap-4'>
@@ -19,7 +24,6 @@ const Hero = () => {
                         <img src="/homepage/call-02.svg" alt="call-icon" />
                     </button>
                 </div>
-                
             </div>
         </section>
     );
